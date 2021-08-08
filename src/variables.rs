43d79591bcb82fa1a3bc55b81682adc68bb6518a
JFortun <josemanuelfortunromero@gmail.com>
2021-08-08 12:16:44 +0200

    Common Programming Concepts - Variables.

---
+++ src/variables.rs
@@ -0,0 +1,18 @@
+pub fn variables() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let y = 2;
+    println!("The value of y is: {}", y);
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("There are {} spaces", spaces);
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+}
