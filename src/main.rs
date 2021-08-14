d2b5da6a6fa3d9c21099271d434362d1efa7b919
JFortun <josemanuelfortunromero@gmail.com>
2021-08-14 20:20:27 +0200

    Guessing game.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod control_flow;
 mod data_types;
 mod functions;
+mod guessing_game;
 mod variables;
 
 fn main() {
     println!("Hello, world!");
-    variables::variables();
-    data_types::data_types();
-    functions::functions();
-    control_flow::control_flow();
+    //variables::variables();
+    //data_types::data_types();
+    //functions::functions();
+    //control_flow::control_flow();
+    guessing_game::_guessing_game();
 }
