0df94ef3950c51a05f4117aa441cdeb75462630e
JFortun <josemanuelfortunromero@gmail.com>
2021-08-24 21:22:15 +0200

    Method Syntax.

--- src/structs.rs
+++ src/structs.rs
@@ -15,6 +15,23 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 pub fn structs() {
     let mut user1 = User {
         _username: String::from("Kevin"),
@@ -43,9 +60,23 @@ pub fn structs() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
     println!("rect1: {:#?}", rect1);
 
-    println!("The area of the rectangle is {}", area(&rect1));
+    println!("The area of the rectangle is {}", rect1.area());
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(5);
+    println!("square: {:#?}", square);
 }
 
 fn _build_user(_username: String, _email: String) -> User {
@@ -55,8 +86,4 @@ fn _build_user(_username: String, _email: String) -> User {
         _sign_in_count: 1,
         _active: true,
     }
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
