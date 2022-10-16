37a2b6743a4f574198b6206c45ac3e4981bf92af
JFortun <josemanuelfortunromero@gmail.com>
2022-10-16 19:49:21 +0200

    Use new root function that returns the response

--- axum_framework/src/main.rs
+++ axum_framework/src/main.rs
@@ -3,11 +3,16 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-    let address = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let app = Router::new()
+        .route("/", get(root));
 
+    let address = SocketAddr::from(([0, 0, 0, 0], 8080));
     axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
