f0f5272143e28f9a264bedca1083f86256b03989
JFortun <josemanuelfortunromero@gmail.com>
2022-10-16 19:37:54 +0200

    Use SocketAddr instead of string for the URL

--- axum_framework/src/main.rs
+++ axum_framework/src/main.rs
@@ -1,10 +1,12 @@
+use std::net::SocketAddr;
 use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let address = SocketAddr::from(([0, 0, 0, 0], 8080));
 
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+    axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
         .unwrap();
