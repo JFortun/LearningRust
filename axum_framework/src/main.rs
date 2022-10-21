ddc8c54033be367ff14bec59064ad41578f5bd88
JFortun <josemanuelfortunromero@gmail.com>
2022-10-21 18:20:14 +0200

    Use tower-http for Cors Layer

--- axum_framework/src/main.rs
+++ axum_framework/src/main.rs
@@ -1,10 +1,16 @@
 use std::net::SocketAddr;
 use axum::{routing::get, Router};
+use axum::http::Method;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root)).layer(cors);
 
     let address = SocketAddr::from(([0, 0, 0, 0], 8080));
     axum::Server::bind(&address)
